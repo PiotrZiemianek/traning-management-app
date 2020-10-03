a138e807de8c2ef231c84374373ec1b4eb354c8e
Piotr Ziemianek <piotr.ziemianek@gmail.com>
2020-10-03 10:31:19 +0200

    SpringBoot project initialized

---
+++ src/test/java/pl/sda/traningmanagementapp/TraningManagementAppApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.sda.traningmanagementapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TraningManagementAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
