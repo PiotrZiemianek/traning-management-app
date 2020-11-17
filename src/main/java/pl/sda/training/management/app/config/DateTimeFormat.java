8bf9d3f3b55c39beb55e969765b929776b5258aa
Piotr Ziemianek <piotr.ziemianek@gmail.com>
2020-11-17 09:48:21 +0100

    Added spring-boot-configuration-processor to pom.xml. Added configuration properties for DateTimeFormat.

--- src/main/java/pl/sda/training/management/app/config/DateTimeFormat.java
+++ src/main/java/pl/sda/training/management/app/config/DateTimeFormat.java
@@ -1,10 +1,12 @@
 package pl.sda.training.management.app.config;
 
-import lombok.Getter;
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
-@Getter
+@Data
 @Component
+@ConfigurationProperties(prefix = "training.date")
 public class DateTimeFormat {
     private String format = "dd.MM.yyyy HH:mm";
 }
